e7c70ab3242756a37ff07a15a698bad4755c1ae5
Caden Huffman <cadenhuf@bu.edu>
2023-08-16 00:10:36 -0400

    Logic completed

--- Edutainment/ContentView.swift
+++ Edutainment/ContentView.swift
@@ -21,66 +21,105 @@ struct ContentView: View {
         }
         
     }
-    @State private var test = "awdf"
+    
     @State private var table =  5
-    @State private var numOfTurns = 5
+    @State private var numOfTurns = 0
     @State private var questions = [Question]()
+    @State private var questionNumber = 0
+    @State private var answer = ""
     @State private var gameRunning = false
+    @State private var gameOver = false
     @State private var score = 0
     var body: some View {
-        if gameRunning == false{
-            NavigationView{
-                VStack {
-                    Stepper("Up to \(table) times table", value: $table, in: 2...12, step: 1)
-                    Spacer()
-                    
-                    HStack{
-                        Button("5"){numOfTurns = 5}
-                            .font(numOfTurns == 5 ? .title : .body)
-                        Button("10"){numOfTurns = 10}
-                            .font(numOfTurns == 10 ? .title : .body)
-                        Button("20"){numOfTurns = 20}
-                            .font(numOfTurns == 20 ? .title : .body)
-                            
-                      
-                    }
-                    Spacer()
-                    Spacer()
-                    Button("Start!"){
-                        startGame(turns: numOfTurns)
-                    }
-                    
-
-                }
-                .padding()
-            }
-        }
-        else{
-            NavigationView{
-                VStack{
-                    Text("hello")
-                    
-                    Button("go back"){gameRunning = false}
-                    
-                    TextField("amount", text: $test)
-                }
-            }
-        }
-
-        
-    }
     
+        NavigationView{
+            VStack {
+                Stepper("Up to \(table) times table", value: $table, in: 2...12, step: 1)
+                Spacer()
+                
+                HStack{
+                    Button("5"){numOfTurns = 5}
+                        .font(numOfTurns == 5 ? .title : .body)
+                    Button("10"){numOfTurns = 10}
+                        .font(numOfTurns == 10 ? .title : .body)
+                    Button("20"){numOfTurns = 20}
+                        .font(numOfTurns == 20 ? .title : .body)
+                        
+                  
+                }
+                Spacer()
+                Spacer()
+                if gameRunning == true{
+                    HStack{
+                        TextField("\(questions[questionNumber].x) * \(questions[questionNumber].y) = ?", text: $answer)
+                        
+                        Button("Submit!"){
+                            checkAnswer()
+                        }
+                    }
+                }
+                Spacer()
+                Spacer()
+                Text("\(score)")
+                Button("Start!"){
+                    startGame(turns: numOfTurns)
+                    
+                }
+                
+
+            }
+            .padding()
+        }
+        .alert("Game Over", isPresented: $gameOver ){
+            Button("Restart", action:restartGame)
+        }message: {
+            Text("Total score is \(score). Would you like to play again?")
+        }
+}
+
+    
+        
+
+
     func startGame(turns:Int){
         
         for _ in 1...turns{
             let question = Question(x: Int.random(in: 1...table), y: Int.random(in:1...table))
             questions.append(question)
         }
+        questionNumber = Int.random(in: 0..<turns)
         gameRunning = true
     }
-    func pickQuestion(){
-        print("hi")
+    func checkAnswer(){
+        if answer == String(questions[questionNumber].xy){
+            score += 1
+            nextTurn()
+        }else{
+            nextTurn()
+        }
     }
+    func nextTurn(){
+        questions.remove(at: questionNumber)
+        if questions.count == 0{
+            gameRunning = false
+            gameOver = true
+            return
+        }
+        questionNumber = Int.random(in: 0..<questions.count)
+        answer = ""
+
+    }
+    
+    func restartGame(){
+        table =  5
+        numOfTurns = 0
+        questions = [Question(x: 5, y: 4)]
+        questionNumber = 0
+        answer = ""
+        gameRunning = false
+        score = 0
+    }
+
     
     
 }
